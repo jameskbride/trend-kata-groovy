c5fe28b0e3d8b7259a672a4c1dbf7d44aace13e2
Jim Kirkbride <james.kirkbride@gmail.com>
2015-11-15 13:57:42 -0500

    Pair (p1, p2) where p1 equals p2 returns 0.0

--- src/main/groovy/TrendCalculator.groovy
+++ src/main/groovy/TrendCalculator.groovy
@@ -9,7 +9,6 @@ class TrendCalculator {
             return 0
         }
 
-        1.0
-
+        values[0] == values[1] ? 0.0 : 1.0
     }
 }
--- src/test/groovy/TrendCalculatorSpec.groovy
+++ src/test/groovy/TrendCalculatorSpec.groovy
@@ -35,4 +35,12 @@ class TrendCalculatorSpec extends Specification {
         then:
         assert 1.0 == result
     }
+
+    def "given a pair of values (p1, p2) when p1 equals p2 then return 0.0"() {
+        when:
+        def result = TrendCalculator.calcTrend([0, 0])
+
+        then:
+        assert 0.0 == result
+    }
 }
