31db788bc22f4e2c4b343993928d477e1b5d32fe
Jim Kirkbride <james.kirkbride@gmail.com>
2015-11-15 14:31:31 -0500

    Refactoring

--- src/main/groovy/TrendCalculator.groovy
+++ src/main/groovy/TrendCalculator.groovy
@@ -5,16 +5,18 @@ class TrendCalculator {
             return null
         }
 
-        if (values && values.size() == 1) {
+        if (values.size() == 1 || (values[0] == values[1])) {
             return 0
         }
 
-        if (values[0] == values[1]) {
-            return 0
-        } else if (values[0] == 0 && values[1] > 0) {
+        if (values[0] == 0 && values[1] > 0) {
             return 1.0
         }
 
+        calculatePairTrend(values)
+    }
+
+    private static double calculatePairTrend(values) {
         BigDecimal pairTrend = new BigDecimal((values[1] - values[0]) / values[0])
 
         pairTrend.setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue()
